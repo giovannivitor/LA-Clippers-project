6166c41ce70accc47cb57deedc1556ad2c094586
giovannivitor <giovanni.santos@sptech.school>
2024-05-17 15:25:23 -0300

    adicionando a tela de avaliações

--- Site/app.js
+++ Site/app.js
@@ -1,10 +1,17 @@
-process.env.AMBIENTE_PROCESSO = "desenvolvimento";
-// process.env.AMBIENTE_PROCESSO = "producao";
+// var ambiente_processo = 'producao';
+var ambiente_processo = 'desenvolvimento';
+
+var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
+// Acima, temos o uso do operador ternário para definir o caminho do arquivo .env
+// A sintaxe do operador ternário é: condição ? valor_se_verdadeiro : valor_se_falso
+
+require("dotenv").config({ path: caminho_env });
 
 var express = require("express");
 var cors = require("cors");
 var path = require("path");
-var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3333 : 8080;
+var PORTA_APP = process.env.APP_PORT;
+var HOST_APP = process.env.APP_HOST;
 
 var app = express();
 
@@ -22,12 +29,19 @@ app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/pontuacao", pontuacaoRouter);
 
-
-
-app.listen(PORTA, function () {
-    console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
-    Você está rodando sua aplicação em Ambiente de ${process.env.AMBIENTE_PROCESSO} \n
-    \t\tSe "desenvolvimento", você está se conectando ao banco LOCAL (MySQL Workbench). \n
-    \t\tSe "producao", você está se conectando ao banco REMOTO (SQL Server em nuvem Azure) \n
-    \t\t\t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'`);
+app.listen(PORTA_APP, function () {
+    console.log(`
+    ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
+    ##   ##  ##       ##  ##            ## ##     ####      ##      ####             ##  ##     ##         ##  
+    ##   ##  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##        ##   
+    ## # ##  ####     #####    ######   ##  ##   ######     ##     ######   ######   ##  ##     ##       ##    
+    #######  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##      ##     
+    ### ###  ##       ##  ##            ## ##    ##  ##     ##     ##  ##             ####      ##     ##      
+    ##   ##  ######   #####             ####     ##  ##     ##     ##  ##              ##      ####    ######  
+    \n\n\n                                                                                                 
+    Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar .: http://${HOST_APP}:${PORTA_APP} :. \n\n
+    Você está rodando sua aplicação em ambiente de .:${process.env.AMBIENTE_PROCESSO}:. \n\n
+    \tSe .:desenvolvimento:. você está se conectando ao banco local. \n
+    \tSe .:producao:. você está se conectando ao banco remoto. \n\n
+    \t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'\n\n`);
 });
--- Site/src/database/config.js
+++ Site/src/database/config.js
@@ -1,71 +1,38 @@
 var mysql = require("mysql2");
-var sql = require('mssql');
 
-// CONEXÃO DO SQL SERVER - AZURE (NUVEM)
-var sqlServerConfig = {
-    server: "SEU_SERVIDOR",
-    database: "SEU_BANCO_DE_DADOS",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
-    pool: {
-        max: 10,
-        min: 0,
-        idleTimeoutMillis: 30000
-    },
-    options: {
-        encrypt: true, // for azure
-    }
-}
-
-// CONEXÃO DO MYSQL WORKBENCH
+// CONEXÃO DO BANCO MYSQL SERVER
 var mySqlConfig = {
-    host: "localhost",
-    database: "Clippers",
-    user: "root",
-    password: "budis2005",
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 };
 
 function executar(instrucao) {
-    // VERIFICA A VARIÁVEL DE AMBIENTE SETADA EM app.js
-    if (process.env.AMBIENTE_PROCESSO == "producao") {
-        return new Promise(function (resolve, reject) {
-            sql.connect(sqlServerConfig).then(function () {
-                return sql.query(instrucao);
-            }).then(function (resultados) {
-                console.log(resultados);
-                resolve(resultados.recordset);
-            }).catch(function (erro) {
-                reject(erro);
-                console.log('ERRO: ', erro);
-            });
-            sql.on('error', function (erro) {
-                return ("ERRO NO SQL SERVER (Azure): ", erro);
-            });
-        });
-    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        return new Promise(function (resolve, reject) {
-            var conexao = mysql.createConnection(mySqlConfig);
-            conexao.connect();
-            conexao.query(instrucao, function (erro, resultados) {
-                conexao.end();
-                if (erro) {
-                    reject(erro);
-                }
-                console.log(resultados);
-                resolve(resultados);
-            });
-            conexao.on('error', function (erro) {
-                return ("ERRO NO MySQL WORKBENCH: ", erro.sqlMessage);
-            });
-        });
-    } else {
-        return new Promise(function (resolve, reject) {
-            console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
-            reject("AMBIENTE NÃO CONFIGURADO EM app.js")
-        });
+
+    if (process.env.AMBIENTE_PROCESSO !== "producao" && process.env.AMBIENTE_PROCESSO !== "desenvolvimento") {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM .env OU dev.env OU app.js\n");
+        return Promise.reject("AMBIENTE NÃO CONFIGURADO EM .env");
     }
+
+    return new Promise(function (resolve, reject) {
+        var conexao = mysql.createConnection(mySqlConfig);
+        conexao.connect();
+        conexao.query(instrucao, function (erro, resultados) {
+            conexao.end();
+            if (erro) {
+                reject(erro);
+            }
+            console.log(resultados);
+            resolve(resultados);
+        });
+        conexao.on('error', function (erro) {
+            return ("ERRO NO MySQL SERVER: ", erro.sqlMessage);
+        });
+    });
 }
 
 module.exports = {
     executar
-}
+};
